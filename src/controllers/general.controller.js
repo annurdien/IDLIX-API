ebabfa696902916636be10d61ce4999adcdd8715
annurdien <ajwadax@gmail.com>
2021-05-29 06:55:07 +0700

    Add cinemaxxi movie list newest

--- src/controllers/general.controller.js
+++ src/controllers/general.controller.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
-const cheerio = require("cheerio");
-const Axios = require("../../config/tools");
-const cache = require("../../config/database");
-const cacheTime = require("../../cacheTime.json");
+const cheerio = require('cheerio');
+const Axios = require('../../config/tools');
+const cache = require('../../config/database');
+const cacheTime = require('../../cacheTime.json');
 
 exports.status = async (_, res) => {
   try {
-    let data = await Axios("/");
+    let data = await Axios('/');
     res.send({
       success: true,
       statusCode: data.status,
@@ -21,34 +21,34 @@ exports.status = async (_, res) => {
 exports.featured = async (_, res) => {
   try {
     /* Get data from cache*/
-    const caches = await cache.featured.get("featured");
+    const caches = await cache.featured.get('featured');
     const hit =
-      Date.now() - (caches?.timestamp || 0) < cacheTime.rekomendasi * 3600000
+      Date.now() - (caches?.timestamp || 0) < cacheTime.featured * 3600000
         ? true
         : false;
     if (hit) return res.send(caches.data)
 
     /* Get Data */
-    const response = await Axios("/");
+    const response = await Axios('/');
     const $ = cheerio.load(response.data);
-    const element = $(".items.featured");
+    const element = $('.items.featured');
 
     /* find n each data */
     let featured = [];
     let title, link;
 
     $(element)
-      .find(".item.movies")
+      .find('.item.movies')
       .each((_, e) => {
-        title = $(e).find(".poster > img").attr("alt");
+        title = $(e).find('.poster > img').attr('alt');
 
         link = {
           endpoint: $(e)
-            .find(".data.dfeatur > h3 > a")
-            .attr("href")
-            .replace("https://185.231.223.71/movie/", ""),
-          url: $(e).find(".data.dfeatur > h3 > a").attr("href"),
-          thumbnail: $(e).find(".poster > img").attr("data-src"),
+            .find('.data.dfeatur > h3 > a')
+            .attr('href')
+            .replace('https://185.231.223.71/movie/', ''),
+          url: $(e).find('.data.dfeatur > h3 > a').attr('href'),
+          thumbnail: $(e).find('.poster > img').attr('data-src'),
         };
 
         console.log(link);
@@ -59,14 +59,72 @@ exports.featured = async (_, res) => {
         });
       });
 
-    await cache.featured.set("featured", {
+    await cache.featured.set('featured', {
 
       data: featured,
       timestamp: Date.now(),
 
     });
 
-    const cacheData = cache.featured.get("featured");
+    const cacheData = cache.featured.get('featured');
+    res.send(cacheData.data);
+
+  } catch (err) {
+
+    res.send({ success: false, error: err.error });
+
+  }
+};
+
+exports.cinemaxxi = async (_, res) => {
+  try {
+    /* Get data from cache*/
+    const caches = await cache.featured.get('cinemaxxi');
+    const hit =
+      Date.now() - (caches?.timestamp || 0) < cacheTime.cinemaxxi * 3600000
+        ? true
+        : false;
+    if (hit) return res.send(caches.data)
+
+    /* Get Data */
+    const response = await Axios('/');
+    const $ = cheerio.load(response.data);
+    const element = $('.items.normal');
+
+    /* find n each data */
+    let cinemaxxi = [];
+    let title, link;
+
+    $(element)
+      .find('.item.movies')
+      .each((_, e) => {
+        title = $(e).find('.poster > img').attr('alt');
+
+        link = {
+          endpoint: $(e)
+            .find('.data > h3 > a')
+            .attr('href')
+            .replace('https://185.231.223.71/movie/', ''),
+          url: $(e).find('.data > h3 > a').attr('href'),
+          thumbnail: $(e).find('.poster > img').attr('data-src'),
+        };
+
+        console.log(link);
+
+        cinemaxxi.push({
+          title,
+          link,
+        });
+      });
+
+    await cache.cinemaxxi.set('cinemaxxi', {
+
+      data: cinemaxxi,
+      timestamp: Date.now(),
+
+    });
+
+    const cacheData = cache.cinemaxxi.get('cinemaxxi');
     res.send(cacheData.data);
 
   } catch (err) {
--- src/routes/general.routes.js
+++ src/routes/general.routes.js
@@ -4,5 +4,6 @@ const generalController = require("../controllers/general.controller");
 /* Home */
 router.get('/', generalController.status);
 router.get('/featured', generalController.featured);
+router.get('/cinemaxxi', generalController.cinemaxxi);
 
 module.exports = router;
